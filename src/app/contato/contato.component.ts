4c92482d882e8398c7b9c003a7a9467c98fb380b
marka <marka@LAPTOP-INOCT3S4>
2020-05-12 14:52:03 -0300

    Crição da página contato

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-contato',
@@ -6,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+
+  faUser = faUser
+  faThumbtack = faThumbtack
+  faEnvelopeOpenText = faEnvelopeOpenText
+  faPhoneAlt = faPhoneAlt
 
   constructor() { }
 
